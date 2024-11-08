de04a27173253aa72e38057143671f689284bb91
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-11-08 10:20:26 +0700

    Updated the description.

--- constants/app.ts
+++ constants/app.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "mersui";
-export const APP_DESCRIPTION = "get rewarded in Sui for your content";
+export const APP_DESCRIPTION = "get rewarded for your content in Sui";
 export const AUTHOR_NAME = "suiware";
 export const AUTHOR_URL = "https://suiware.io";
