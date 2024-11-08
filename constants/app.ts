1d9b9d615a494c009424a2da6c02f02168fa4761
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-11-08 09:05:07 +0700

    Added project name and description.

---
+++ constants/app.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = "mersui";
+export const APP_DESCRIPTION =
+  "A quick way to get rewards for your content work in Sui";
+export const AUTHOR_NAME = "suiware";
+export const AUTHOR_URL = "https://suiware.io";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import { APP_DESCRIPTION, APP_NAME } from "~~/constants/app";
+import Link from "next/link";
+import {
+  APP_DESCRIPTION,
+  APP_NAME,
+  AUTHOR_NAME,
+  AUTHOR_URL,
+} from "~~/constants/app";
 
 export default function Home() {
   return (
     <div>
       <main className="flex flex-col justify-center items-center h-screen">
         <h1 className="text-4xl font-bold">{APP_NAME}</h1>
-        <h2 className="text-xl">{APP_DESCRIPTION}</h2>
+        <h2 className="text-xl mt-1">{APP_DESCRIPTION}</h2>
+
+        <div className="mt-5 text-center">
+          by{" "}
+          <Link className="text-indigo-500" href={AUTHOR_URL} target="_blank">
+            @{AUTHOR_NAME}
+          </Link>
+        </div>
       </main>
     </div>
   );
