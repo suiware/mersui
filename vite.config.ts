62cfe75d663da91e229b12a655d606a46f81cf31
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-11-09 10:57:44 +0700

    Continued configuring the library.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ tsconfigPath: "./tsconfig.lib.json" })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ tsconfigPath: "./tsconfig.lib.json" }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
@@ -14,6 +19,10 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
