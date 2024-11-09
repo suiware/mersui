65a7b52558542a27046bf40d13cc909be84a688a
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-11-09 11:57:54 +0700

    Improved configs.

--- lib/components/MersuiWidget.tsx
+++ lib/components/MersuiWidget.tsx
@@ -4,9 +4,9 @@ import {
   SuiClientProvider,
   WalletProvider,
 } from "@mysten/dapp-kit";
-
 import { getFullnodeUrl } from "@mysten/sui/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import '@mysten/dapp-kit/dist/index.css';
 
 // Config options for the networks you want to connect to
 const { networkConfig } = createNetworkConfig({
