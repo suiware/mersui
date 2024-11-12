351a50483d6fc1fa2547037e47187fba53675ef6
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-11-12 22:40:40 +0700

    Working on the widget.

--- lib/components/MersuiWidget.tsx
+++ lib/components/MersuiWidget.tsx
@@ -4,11 +4,26 @@ import {
   useSignAndExecuteTransaction,
 } from "@mysten/dapp-kit";
 import { Transaction } from "@mysten/sui/transactions";
-import { FC, useState } from "react";
+import {
+  FC,
+  MouseEventHandler,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from "react";
 
 interface IMersuiWidget {
   recipientAddress: string;
 }
+
+const fetchSuiPrice = async () => {
+  const response = await fetch(
+    "https://hermes.pyth.network/v2/updates/price/latest?ids%5B%5D=23d7315113f5b1d3ba7a83604c44b94d79f4fd69af77f804fc7f920a6dc65744"
+  );
+  const data = await response.json();
+  console.log(data);
+  return data.parsed[0].price;
+};
 
 export const MersuiWidget: FC<IMersuiWidget> = ({ recipientAddress }) => {
   const currentAccount = useCurrentAccount();
@@ -41,10 +56,20 @@ export const MersuiWidget: FC<IMersuiWidget> = ({ recipientAddress }) => {
     );
   }
 
+  useEffect(() => {
+    if (currentAccount) {
+      fetchSuiPrice().then((price) => {
+        console.log("price", price);
+      });
+    }
+  }, [currentAccount]);
+
   if (currentAccount) {
     return (
       <div>
-        <button onClick={() => performTransaction()}>Pay</button>
+        <MersuiButton onClick={() => performTransaction()}>
+          Mersui $3
+        </MersuiButton>
       </div>
     );
   }
@@ -52,13 +77,31 @@ export const MersuiWidget: FC<IMersuiWidget> = ({ recipientAddress }) => {
   return (
     <ConnectModal
       trigger={
-        <button
-          disabled={!!currentAccount}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md disabled:bg-gray-100 disabled:text-gray-400"
-        >
-          Say MerSui
-        </button>
+        <MersuiButton disabled={!!currentAccount}>Mersui $3</MersuiButton>
       }
     />
+  );
+};
+
+interface IMersuiButton {
+  disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const MersuiButton: FC<PropsWithChildren<IMersuiButton>> = ({
+  children,
+  disabled,
+  onClick,
+}) => {
+  return (
+    <div className="flex flex-row items-center justify-center">
+      <button
+        disabled={disabled}
+        className="px-6 py-2 text-gray-700 font-bold text-lg bg-yellow-400 rounded-full shadow-md hover:text-gray-700 hover:bg-yellow-300 border-none"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
   );
 };
